d1700d4c34f2b514a8761f03ff4a1c492a40a0fa
Fiona Wangui <you@example.com>
2021-10-16 15:15:19 +0300

    add gender

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+
+let year= document.getElementById("YEAR").value;
+let month= document.getElementById("MONTH").value;
+let date= document.getElementById("DATE").value;
+
+let gender=document.getElementById("gender"); 
