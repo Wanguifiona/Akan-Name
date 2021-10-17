3cc4f1653b28d92cc254df7e14a8e8c1d2622fff
Fiona Wangui <you@example.com>
2021-10-17 11:47:04 +0300

    added part of script.js

---
+++ js/script.js
@@ -0,0 +1,51 @@
+let captureDate;
+let capturedGender;
+let akanName;
+document.getElementById("dateInput").addEventListener("change", function() {
+ const input = this.value;
+ console.log(input); //e.g. 2015-11-13
+ captureDate=input
+})
+
+
+function getDate() {
+  let date=document.getElementById("dateInput")
+  console.log("date")
+  const dateOfBirth = new Date(captureDate)
+  const capturedMonth= dateOfBirth.getMonth()
+  const capturedDate=dateOfBirth.getDate()
+  const capturedYear= dateOfBirth.getFullYear()
+
+     console.log(capturedMonth, capturedYear)
+     if(document.getElementById('male').checked){
+       capturedGender="Male"
+
+     }
+     else if (document.getElementById('female').checked){
+       capturedGender="Female"
+
+     }
+     console.log(captureDate, capturedGender)
+     const CC=parseInt(capturedYear.toString().slice(2,4))
+     const YY= parseInt(capturedYear.toString().slice(0,2))
+     const MM=capturedMonth+1
+     const DD=capturedDate+1
+
+     const dateOfWeek=  Math.ceil(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7)
+     console.log(dateOfWeek)
+
+     if(capturedGender==="Male" && dateOfWeek===1){
+       console.log("Kwasi")
+       akanName="Kwasi"
+     }
+     else if (capturedGender==="Male" && dateOfWeek===2){
+       console.log("Kwadwo")
+       akanName="Kwadwo"
+     }
+
+     alert("Your Akan name is ", akanName)
+
+
+
+
+}
