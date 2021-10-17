ff601cb7d20d82ea29f467485a765251fa6db466
Fiona Wangui <you@example.com>
2021-10-17 11:36:21 +0300

    new gender statement

--- js/script.js
+++
@@ -1,7 +0,0 @@
-
-
-let year= document.getElementById("YEAR").value;
-let month= document.getElementById("MONTH").value;
-let date= document.getElementById("DATE").value;
-
-let gender=document.getElementById("gender"); 
